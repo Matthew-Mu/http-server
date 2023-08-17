4a45ab694f8d35b588dda5628a9f0b0e3fbe5eb8
matthewmu <matthewmu@openSUSE.gateway>
2023-08-17 21:05:02 +1000

    moving some files around

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fileserver := http.FileServer(http.Dir("./static"))
+
+	http.Handle("/", fileserver)
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/form", formHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
